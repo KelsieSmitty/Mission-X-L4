b27adc991f1cdf2a74641780adcb01522e323b26
Luis Rodriguez <luis@missionreadyhq.com>
2023-06-12 21:46:52 +1200

    Fist commit, Mui Framework and button

---
+++ src/Components/MuiButton.js
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+export default function MuiButton( ) {
+  return (
+    <div>
+      <a href="https://www.google.com/">
+        <Button variant="contained" size="large" color="secondary">
+          Contained
+        </Button>
+        Visit Google!
+      </a>
+    </div>
+  );
+}
