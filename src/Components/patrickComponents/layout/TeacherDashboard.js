60e45ad79b6dba7ddaa5c1b73fceebd111fac0c8
John Doe <patricktricenio549@gmail.com>
2023-06-20 10:48:40 +1200

    Added responsive downloader when clicked

--- src/Components/patrickComponents/layout/TeacherDashboard.js
+++ src/Components/patrickComponents/layout/TeacherDashboard.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import MainDashboard from '../../../Pages/MainDashboard';
 import styles from '../../../Styling/TeacherDashboard.module.css';
+import images from '../../../images/StudentDashboard/VideoTutorial.png';
 
 const TeacherDashboard = () => {
+  const alert7 = () => {
+    return alert('completed sucessfully');
+  };
   return (
     <MainDashboard>
       <div id={styles['Container2']}>
         <h2 id={styles['ProjectSubmission']}>Project Submissions</h2>
-        <button id={styles['downloadbutton']}>Mark as complete project</button>
-        <button id={styles['downloadbutton']}>Download Files</button>
+        <button id={styles['downloadbutton']} onClick={alert7}>
+          Mark as complete project
+        </button>
+        <a
+          href={images}
+          download="Aiden Project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button id={styles['downloadbutton']}>Download Files</button>
+        </a>
         <div id={styles['Aiden1']}>
           <input type="checkbox" id="nav-check"></input>
           Aiden Submitted his project
