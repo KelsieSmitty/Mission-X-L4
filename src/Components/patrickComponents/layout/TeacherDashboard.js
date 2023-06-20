3e3130f1afe72df52928f32be6350bc5bde3e3cd
John Doe <patricktricenio549@gmail.com>
2023-06-20 14:54:20 +1200

    Added translation on Teacher Dashboard

--- src/Components/patrickComponents/layout/TeacherDashboard.js
+++ src/Components/patrickComponents/layout/TeacherDashboard.js
@@ -2,15 +2,38 @@ import React from 'react';
 import MainDashboard from '../../../Pages/MainDashboard';
 import styles from '../../../Styling/TeacherDashboard.module.css';
 import images from '../../../images/StudentDashboard/VideoTutorial.png';
+import { useEffect, useState } from 'react';
+import TranslatedLanguage2 from '../../../Components/patrickComponents/layout/TeacherDashboardData.json';
 
 const TeacherDashboard = () => {
   const alert7 = () => {
     return alert('completed sucessfully');
   };
+
+  const [language, setLanguage] = useState('english');
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (language === 'english') {
+      setContent(TranslatedLanguage2.english);
+    } else if (language === 'Maori') {
+      setContent(TranslatedLanguage2.Maori);
+    }
+  });
+
   return (
     <MainDashboard>
       <div id={styles['Container2']}>
         <h2 id={styles['ProjectSubmission']}>Project Submissions</h2>
+        <select
+          value={language}
+          onChange={(e) => {
+            setLanguage(e.target.value);
+          }}
+        >
+          <option>English</option>
+          <option>Maori</option>
+        </select>
         <button id={styles['downloadbutton']} onClick={alert7}>
           Mark as complete project
         </button>
@@ -24,25 +47,25 @@ const TeacherDashboard = () => {
         </a>
         <div id={styles['Aiden1']}>
           <input type="checkbox" id="nav-check"></input>
-          Aiden Submitted his project
+          {content.Description}
           <div id={styles['time3']}>
-            <div>Thurs July 2023</div>
+            <div>{content.Description2}</div>
             <div>10:53 Am</div>
           </div>
         </div>
         <div id={styles['Rawiri1']}>
           <input type="checkbox" id="nav-check"></input>
-          Rawiri wants to show his project
+          {content.Description3}
           <div id={styles['time3']}>
-            <div> Wed July 2023</div>
+            <div>{content.Description4}</div>
             <div>10:52 Am</div>
           </div>
         </div>
         <div id={styles['Neevah1']}>
           <input type="checkbox" id="nav-check"></input>
-          Neevah wants to show her project
+          {content.Description5}
           <div id={styles['time3']}>
-            <div>Tue July 2023</div>
+            <div>{content.Description6}</div>
             <div>10:50 Am</div>
           </div>
         </div>
