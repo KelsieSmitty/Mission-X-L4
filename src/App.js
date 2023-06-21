909afdd4bd3c723e96de71b5aa806acd18585b0c
Kelsie Smith <kelsies@missionreadyhq.com>
2023-06-22 11:01:33 +1200

    updated home page 22/6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import "./App.css";
-import { HomeContent } from "./Components/KSComponents/HomeContent";
-import NavBar from "./Components/KSComponents/NavBar";
-import Footer from "./Components/KSComponents/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-  return (
-    <Router>
-      <NavBar />
-      <HomeContent />
-      {/* <Footer /> */}
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,14 @@
-import HomeNavBar from "../Components/KSComponents/HomeNavBar";
+import { HomeContent } from "../Components/KSComponents/HomeContent";
+import NavBar from "../Components/KSComponents/NavBar";
+import Footer from "../Components/KSComponents/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Home() {
-  return <div>NAVBAR BANNER CONTENT FOOTER</div>;
+  return (
+    <Router>
+      <NavBar />
+      <HomeContent />
+      {/* <Footer /> */}
+    </Router>
+  );
 }
