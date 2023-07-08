f1412440f602b0023ab58d5f5c534cc208df6dbb
Luis Rodriguez <luis@missionreadyhq.com>
2023-07-08 16:11:11 +1200

    test page for rendering student id from server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import StudentProjectLibrary from "./pages/StudentProjectLibrary";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentProfileViewer from "./pages/StudentProfileViewer";
-
+import Test2SPV from "./pages/Test2SPV"
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/student-project" element={<StudentProjectLibrary />} />
           <Route path="/student-profile-viewer" element={<StudentProfileViewer />} />
+          <Route path="/dynamic1" element={< Test2SPV/>} />
         </Routes>
       </BrowserRouter>
     </div>
